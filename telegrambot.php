b2e4b196719375134f80a709fa0c612d027f0771
Sergey Bunin <sebun@ya.ru>
2021-12-10 20:30:24 +0300

    Create telegrambot.php

---
+++ telegrambot.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ *   TelegramBot Plugin 2.0
+ *   Copyright (C) 2021  Sergey Bunin
+ *   Released under GNU General Public License version 3 or later
+ */
+defined('_JEXEC') or die;
+
+class PlgContentTelegrambot extends JPlugin
+{
+	/* Load language */
+	protected $autoloadLanguage = true;
+	
+	/* Set data */
+	public function onContentAfterSave($context, $article, $isNew)
+	{
+		$link = $this->params->get('telegrambot_link', false);
+		$token = $this->params->get('telegrambot_token', false);
+		$channel = $this->params->get('telegrambot_channel', false);
+		
+		$this->sendTelegram($link, $token, $channel, $article);	
+	}
+  
+	/* Send to telegram channel */
+	public function sendTelegram($link, $token, $channel, $article)
+	{
+		$url = 'https://api.telegram.org/bot' . $token . '/sendPhoto';
+		$text = $article->title;
+		$images  = json_decode($article->get("images"));
+		$image   = $images->image_intro;
+
+		$inlinekeys[] = array(
+							array(
+								"text" => JText::_('PLG_TELEGRAMBOT_CHANNEL_SITEBUTTON'),
+								"url" => JRoute::_($link . "index.php?option=com_content&view=article&id=".$article->id)
+							)
+						);
+		$inlinekeyboard = array("inline_keyboard" => $inlinekeys);
+		
+		$content = array(
+						"chat_id" => $channel,
+						"caption" => $text,
+						"photo" => $link . $image,
+						"reply_markup" => $inlinekeyboard
+					);
+					
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($content));
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	}
+}
